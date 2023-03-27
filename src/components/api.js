de78addd364ed7ebebd4e32ab7a6fec92928b51d
Samy Taha <samytaha123@gmail.com>
2023-03-27 11:51:02 +0000

    updated each leg and it's stop

--- src/components/api.js
+++ src/components/api.js
@@ -4,8 +4,6 @@ import { useState, useEffect } from "react";
 import "../api.css";
 import logo from "../logoTFL.svg";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 export function TfLJourneyPlanner() {
   const [from, setFrom] = useState("");
   const [destination, setDestination] = useState("");
@@ -15,14 +13,9 @@ export function TfLJourneyPlanner() {
   const [arrival, journeyArrives] = useState("");
   const [departure, journeyDeparts] = useState("");
   const [lines, setLines] = useState("");
-  
   const [legs, setLegs] = useState([]);
   const [legArrivals, setLegArrivals] = useState([]);
-  const [stops, setStops] = useState([])
-  
-
-
-  
+  const [stops, setStops] = useState([]);
 
   function getJourney() {
     fetch(
@@ -30,28 +23,19 @@ export function TfLJourneyPlanner() {
     )
       .then((response) => response.json())
       .then((data) => {
-        
-
         setJourneys(data.journeys);
         // console.log(journeys)
 
-        setStops(data.journeys[0].legs[0].path.stopPoints)
-
-      
-
-        
+        setStops(data.journeys[0].legs[0].path.stopPoints);
 
         setLegs(data.journeys[0].legs);
 
-        // console.log(legArrivals);
-
-        // console.log(legs);
+        for (let i = 0; i < journeys.length; i++) {
+          console.log(journeys[i].legs[i].path);
+        }
 
         setLines(data.journeys[0].legs[0].routeOptions[0].name);
         // console.log(lines);
-
-        
-
 
         let fareTotal = data.journeys[0].fare.totalCost;
         let total = fareTotal / 100;
@@ -91,8 +75,9 @@ export function TfLJourneyPlanner() {
         <img src={logo} alt="logo" className="logo" />
       </div>
 
+      {/* Start of journey form  */}
       <div className="journeyContainer">
-          <h2>Plan a journey</h2>
+        <h2>Plan a journey</h2>
         <form>
           <label>From:</label>
           <select
@@ -127,124 +112,200 @@ export function TfLJourneyPlanner() {
           </button>
         </form>
       </div>
+      {/* End of Journey form  */}
 
-      <div className="resultsContainer" style={{
-        backgroundColor: departure ? "white" : null
-      }}>
-                
-                <div
-                    className="lineImage"
-                    style={{
-                    backgroundColor:
-                      lines === "Central" ? "#E32017": 
-                      lines === "Bakerloo"? "#B36305": 
-                      lines === "Metropolitan"? "#9B0056": 
-                      lines === "Piccadilly"? "#003688": 
-                      lines === "Northern"? "#000000": 
-                      lines === "District"? "#00782A": 
-                      lines === "Jubilee"? "#A0A5A9": 
-                      lines === "Victoria"? "#0098D4": 
-                      lines === "Circle"? "#FFD300": 
-                      lines === "Elizabeth"? "#6950a1": 
-                      lines === "Hammersmith & City"? "#F3A9BB": 
-                      lines === "Waterloo & City"? "#95CDBA": 
-                      lines === "Chiltern Railways"? "rgba(0, 0, 0, 0.5)": null}}
-                    >
-                    {lines ? <span>{lines} line</span> : ""}
-                  </div>
-                  
+      {/* Start of results list  */}
+      <div
+        className="resultsContainer"
+        style={{
+          backgroundColor: departure ? "white" : null,
+        }}
+      >
+        {/* Line Tag  */}
+        <div
+          className="lineTag"
+          style={{
+            backgroundColor:
+              lines === "Central"
+                ? "#E32017"
+                : lines === "Bakerloo"
+                ? "#B36305"
+                : lines === "Metropolitan"
+                ? "#9B0056"
+                : lines === "Piccadilly"
+                ? "#003688"
+                : lines === "Northern"
+                ? "#000000"
+                : lines === "District"
+                ? "#00782A"
+                : lines === "Jubilee"
+                ? "#A0A5A9"
+                : lines === "Victoria"
+                ? "#0098D4"
+                : lines === "Circle"
+                ? "#FFD300"
+                : lines === "Elizabeth"
+                ? "#6950a1"
+                : lines === "Hammersmith & City"
+                ? "#F3A9BB"
+                : lines === "Waterloo & City"
+                ? "#95CDBA"
+                : lines === "Chiltern Railways"
+                ? "rgba(0, 0, 0, 0.5)"
+                : null,
+          }}
+        >
+          {lines ? <span>{lines} line</span> : ""}
+        </div>
 
-        {journeys.map(journey => (
+        {journeys.map((journey) => (
           <div className="journeyResults">
-                
-                <div className="top">
-                <div className="left">{journey.startDateTime.slice(11,16)}</div>
-                <div className="middle">
-                  <hr style={{
+            <div className="journey">
+              <div className="left">{journey.startDateTime.slice(11, 16)}</div>
+              <div className="middle">
+                <hr
+                  style={{
                     color:
-                      lines === "Central" ? "#E32017": 
-                      lines === "Bakerloo"? "#B36305": 
-                      lines === "Metropolitan"? "#9B0056": 
-                      lines === "Piccadilly"? "#003688": 
-                      lines === "Northern"? "#000000": 
-                      lines === "District"? "#00782A": 
-                      lines === "Jubilee"? "#A0A5A9": 
-                      lines === "Victoria"? "#0098D4": 
-                      lines === "Circle"? "#FFD300": 
-                      lines === "Elizabeth"? "#6950a1": 
-                      lines === "Hammersmith & City"? "#F3A9BB": 
-                      lines === "Waterloo & City"? "#95CDBA": 
-                      lines === "Chiltern Railways"? "rgba(0, 0, 0, 0.5)": null}}
-                  />
-                  <div
-                    className="lineImage"
-                    style={{
-                    borderColor:lines === "Central" ? "#E32017": lines === "Bakerloo"? "#B36305": lines === "Metropolitan"? "#9B0056": lines === "Piccadilly"? "#003688": lines === "Northern"? "#000000": lines === "District"? "#00782A": lines === "Jubilee"? "#A0A5A9": lines === "Victoria"? "#0098D4": lines === "Circle"? "#FFD300": lines === "Elizabeth"? "#6950a1": lines === "Hammersmith & City"? "#F3A9BB": lines === "Waterloo & City"? "#95CDBA": lines === "Chiltern Railways"? "rgba(0, 0, 0, 0.5)": null,
-                    color:lines === "Central" ? "#E32017": lines === "Bakerloo"? "#B36305": lines === "Metropolitan"? "#9B0056": lines === "Piccadilly"? "#003688": lines === "Northern"? "#000000": lines === "District"? "#00782A": lines === "Jubilee"? "#A0A5A9": lines === "Victoria"? "#0098D4": lines === "Circle"? "#FFD300": lines === "Elizabeth"? "#6950a1": lines === "Hammersmith & City"? "#F3A9BB": lines === "Waterloo & City"? "#95CDBA": lines === "Chiltern Railways"? "rgba(0, 0, 0, 0.5)": null}}
-                  >
+                      lines === "Central"
+                        ? "#E32017"
+                        : lines === "Bakerloo"
+                        ? "#B36305"
+                        : lines === "Metropolitan"
+                        ? "#9B0056"
+                        : lines === "Piccadilly"
+                        ? "#003688"
+                        : lines === "Northern"
+                        ? "#000000"
+                        : lines === "District"
+                        ? "#00782A"
+                        : lines === "Jubilee"
+                        ? "#A0A5A9"
+                        : lines === "Victoria"
+                        ? "#0098D4"
+                        : lines === "Circle"
+                        ? "#FFD300"
+                        : lines === "Elizabeth"
+                        ? "#6950a1"
+                        : lines === "Hammersmith & City"
+                        ? "#F3A9BB"
+                        : lines === "Waterloo & City"
+                        ? "#95CDBA"
+                        : lines === "Chiltern Railways"
+                        ? "rgba(0, 0, 0, 0.5)"
+                        : null,
+                  }}
+                />
+                <div
+                  className="lineImage"
+                  style={{
+                    borderColor:
+                      lines === "Central"
+                        ? "#E32017"
+                        : lines === "Bakerloo"
+                        ? "#B36305"
+                        : lines === "Metropolitan"
+                        ? "#9B0056"
+                        : lines === "Piccadilly"
+                        ? "#003688"
+                        : lines === "Northern"
+                        ? "#000000"
+                        : lines === "District"
+                        ? "#00782A"
+                        : lines === "Jubilee"
+                        ? "#A0A5A9"
+                        : lines === "Victoria"
+                        ? "#0098D4"
+                        : lines === "Circle"
+                        ? "#FFD300"
+                        : lines === "Elizabeth"
+                        ? "#6950a1"
+                        : lines === "Hammersmith & City"
+                        ? "#F3A9BB"
+                        : lines === "Waterloo & City"
+                        ? "#95CDBA"
+                        : lines === "Chiltern Railways"
+                        ? "rgba(0, 0, 0, 0.5)"
+                        : null,
+                  }}
+                >
                   {lines ? <span>{journey.duration} Mins</span> : ""}
-                  </div>
-
-                  <hr style={{
-                    color:
-                      lines === "Central" ? "#E32017": 
-                      lines === "Bakerloo"? "#B36305": 
-                      lines === "Metropolitan"? "#9B0056": 
-                      lines === "Piccadilly"? "#003688": 
-                      lines === "Northern"? "#000000": 
-                      lines === "District"? "#00782A": 
-                      lines === "Jubilee"? "#A0A5A9": 
-                      lines === "Victoria"? "#0098D4": 
-                      lines === "Circle"? "#FFD300": 
-                      lines === "Elizabeth"? "#6950a1": 
-                      lines === "Hammersmith & City"? "#F3A9BB": 
-                      lines === "Waterloo & City"? "#95CDBA": 
-                      lines === "Chiltern Railways"? "rgba(0, 0, 0, 0.5)": null}}
-                  />
                 </div>
-                
-                <div className="right">{journey.arrivalDateTime.slice(11,16)}</div>
-                
+
+                <hr
+                  style={{
+                    color:
+                      lines === "Central"
+                        ? "#E32017"
+                        : lines === "Bakerloo"
+                        ? "#B36305"
+                        : lines === "Metropolitan"
+                        ? "#9B0056"
+                        : lines === "Piccadilly"
+                        ? "#003688"
+                        : lines === "Northern"
+                        ? "#000000"
+                        : lines === "District"
+                        ? "#00782A"
+                        : lines === "Jubilee"
+                        ? "#A0A5A9"
+                        : lines === "Victoria"
+                        ? "#0098D4"
+                        : lines === "Circle"
+                        ? "#FFD300"
+                        : lines === "Elizabeth"
+                        ? "#6950a1"
+                        : lines === "Hammersmith & City"
+                        ? "#F3A9BB"
+                        : lines === "Waterloo & City"
+                        ? "#95CDBA"
+                        : lines === "Chiltern Railways"
+                        ? "rgba(0, 0, 0, 0.5)"
+                        : null,
+                  }}
+                />
               </div>
 
-              <div className="bottom">{lines ? <span>£{fare}</span> : ""}</div>
+              <div className="right">
+                {journey.arrivalDateTime.slice(11, 16)}
+              </div>
+            </div>
 
+            <div className="fare">{lines ? <span>£{fare}</span> : ""}</div>
+            <div className="legContainer">
+              <div className="routeLine">
+                {/* This section updates the color of the line based on the lines state variable value */}
+
+                {/* This section maps through the different stops of a route leg, and displays it */}
+
+                {arrival ? <h3>Change at</h3> : ""}
+
+                <div className="legArrivals">
+                  {legs.map((leg) => {
+                    console.log(leg.arrivalPoint.commonName);
+                    const arrivalPoint = leg.arrivalPoint.commonName;
+                    const stopPoints = leg.path.stopPoints;
+                    return (
+                      <div key={arrivalPoint}>
+                        {stopPoints.map((stop, i) => {
+                          console.log("stopXYZ", stop);
+                          if (stop.name !== arrivalPoint) {
+                            return <li key={i}>{stop.name}</li>;
+                          } else {
+                            return;
+                          }
+                        })}
+                        <li>
+                          <b>{arrivalPoint}</b>
+                        </li>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
           </div>
         ))}
-
-
-
-
-<div className="legContainer">
-        <div className="routeLine">
-          {/* This section updates the color of the line based on the lines state variable value */}
-          
-
-          {/* This section maps through the different stops of a route leg, and displays it */}
-
-          {arrival ? <h3>Change at</h3> : ""}
-
-          <div className="legArrivals">
-            {legArrivals.map((arrival) => (
-              <div key={arrival}>
-                <li>{arrival}</li>
-              </div>
-            ))}
-
-            
-          </div>
-          <div className="stops">
-              {departure ? <h3>Stopping at</h3> : null}
-              {stops.map(stop => (
-                <div key={stop.name}>
-                  <li>{stop.name}</li>
-                </div>
-              ))}
-            </div>
-        </div>
       </div>
-      </div>
-      
     </div>
   );
 }
